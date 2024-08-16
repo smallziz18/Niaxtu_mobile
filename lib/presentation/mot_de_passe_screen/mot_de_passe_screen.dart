511a8b0071adb7249169e8a5069569c5b945455b
CodeNinja <14mbaye28@gmail.com>
2024-08-16 11:58:03 +0000

    modif_profil

--- lib/presentation/mot_de_passe_screen/mot_de_passe_screen.dart
+++ lib/presentation/mot_de_passe_screen/mot_de_passe_screen.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:niaxtumobile/presentation/identification_screen/identification_screen.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
 import 'package:niaxtumobile/routes/app_routes.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class MotDePasseScreen extends StatelessWidget {
-  MotDePasseScreen({super.key});
+class MotDePasseScreen extends StatefulWidget {
+  @override
+  _MotDePasseScreenState createState() => _MotDePasseScreenState();
+}
 
+class _MotDePasseScreenState extends State<MotDePasseScreen> {
   final TextEditingController reenterPasswordController =
       TextEditingController();
   final TextEditingController confirmPasswordController =
       TextEditingController();
+  File? _profileImage;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _profileImage = File(pickedFile.path);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,40 +34,13 @@ class MotDePasseScreen extends StatelessWidget {
         backgroundColor: const Color(0xFFFFFCFC),
         resizeToAvoidBottomInset: false,
         body: SingleChildScrollView(
-          // Ajouté pour permettre le défilement
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 const SizedBox(height: 16),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: SvgPicture.asset(
-                          "assets/images/img_vector_1.svg",
-                          height: 24,
-                          width: 24,
-                        ),
-                        onPressed: () {
-                          Navigator.pop(context); // Retour à l'écran précédent
-                        },
-                      ),
-                      Expanded(
-                        child: Center(
-                          child: CustomImageView(
-                            imagePath: ImageConstant.imgLogo,
-                            height: 147,
-                            width: 171,
-                            radius: BorderRadius.circular(71),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                _buildHeader(context),
                 const SizedBox(height: 16),
                 const Text(
                   "Création de mot de passe",
@@ -63,66 +51,14 @@ class MotDePasseScreen extends StatelessWidget {
                     fontWeight: FontWeight.w400,
                   ),
                 ),
-                const SizedBox(height: 8),
-                const Text(
-                  "Password",
-                  style: TextStyle(
-                    color: Color(0xFF817C7C),
-                    fontSize: 20,
-                    fontFamily: 'Book Antiqua',
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
+                const SizedBox(height: 16),
+                _buildProfileImagePicker(), // Section ajoutée pour la photo de profil
                 const SizedBox(height: 24),
                 _buildPasswordCreationSection(),
                 const SizedBox(height: 12),
                 _buildConfirmPasswordSection(),
                 const SizedBox(height: 24),
-                Container(
-                  width: double.infinity,
-                  height: 48,
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF29B6F6),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(22),
-                      ),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 30,
-                        vertical: 8,
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, AppRoutes.initialRoute);
-                    },
-                    child: const Text(
-                      "Créer",
-                      style: TextStyle(
-                        color: Color(0xFFFFFFFF),
-                        fontSize: 20,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 15),
-                SizedBox(
-                  height: 13,
-                  child: AnimatedSmoothIndicator(
-                    activeIndex: 1, // Ajuste selon la page active
-                    count: 3,
-                    effect: ScrollingDotsEffect(
-                      spacing: 9,
-                      activeDotColor: const Color(0xFF29B6F6),
-                      dotColor: const Color(0xFFD9D9D9),
-                      activeDotScale: 1.3,
-                      dotHeight: 10,
-                      dotWidth: 10,
-                    ),
-                  ),
-                ),
+                _buildCreateButton(),
                 const SizedBox(height: 16),
               ],
             ),
@@ -132,7 +68,50 @@ class MotDePasseScreen extends StatelessWidget {
     );
   }
 
-  /// Password Creation Section Widget
+  Widget _buildHeader(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Retour à l'écran précédent
+          },
+        ),
+        Expanded(
+          child: Center(
+            child: const Text(
+              "Créer un compte",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// Widget pour le sélecteur d'image de profil
+  Widget _buildProfileImagePicker() {
+    return GestureDetector(
+      onTap: _pickImage,
+      child: CircleAvatar(
+        radius: 50,
+        backgroundColor: Colors.grey[300],
+        backgroundImage:
+            _profileImage != null ? FileImage(_profileImage!) : null,
+        child: _profileImage == null
+            ? const Icon(
+                Icons.camera_alt,
+                size: 40,
+                color: Colors.white,
+              )
+            : null,
+      ),
+    );
+  }
+
   Widget _buildPasswordCreationSection() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
@@ -151,21 +130,9 @@ class MotDePasseScreen extends StatelessWidget {
           const SizedBox(height: 8),
           TextFormField(
             controller: reenterPasswordController,
-            style: const TextStyle(
-              color: Color(0xFF817C7C),
-              fontSize: 16,
-              fontFamily: 'Book Antiqua',
-              fontWeight: FontWeight.w400,
-            ),
             obscureText: true,
             decoration: InputDecoration(
               hintText: "Mot de passe",
-              hintStyle: const TextStyle(
-                color: Color(0xFF817C7C),
-                fontSize: 16,
-                fontFamily: 'Book Antiqua',
-                fontWeight: FontWeight.w400,
-              ),
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(6),
                 borderSide: const BorderSide(
@@ -180,24 +147,6 @@ class MotDePasseScreen extends StatelessWidget {
                   width: 2,
                 ),
               ),
-              filled: true,
-              fillColor: const Color(0xFFFDFBFB),
-              isDense: true,
-              contentPadding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 14,
-              ),
-              suffixIcon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset(
-                  "assets/images/img_group_8.svg",
-                  height: 24,
-                  width: 27,
-                ),
-              ),
-              suffixIconConstraints: const BoxConstraints(
-                maxHeight: 48,
-              ),
             ),
           ),
         ],
@@ -205,7 +154,6 @@ class MotDePasseScreen extends StatelessWidget {
     );
   }
 
-  /// Confirm Password Section Widget
   Widget _buildConfirmPasswordSection() {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
@@ -224,22 +172,9 @@ class MotDePasseScreen extends StatelessWidget {
           const SizedBox(height: 8),
           TextFormField(
             controller: confirmPasswordController,
-            style: const TextStyle(
-              color: Color(0xFF817C7C),
-              fontSize: 16,
-              fontFamily: 'Book Antiqua',
-              fontWeight: FontWeight.w400,
-            ),
-            textInputAction: TextInputAction.done,
             obscureText: true,
             decoration: InputDecoration(
               hintText: "Confirmer votre mot de passe",
-              hintStyle: const TextStyle(
-                color: Color(0xFF817C7C),
-                fontSize: 16,
-                fontFamily: 'Book Antiqua',
-                fontWeight: FontWeight.w400,
-              ),
               enabledBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(6),
                 borderSide: const BorderSide(
@@ -254,27 +189,34 @@ class MotDePasseScreen extends StatelessWidget {
                   width: 2,
                 ),
               ),
-              filled: true,
-              fillColor: const Color(0xFFFDFBFB),
-              isDense: true,
-              contentPadding: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 14,
-              ),
-              suffixIcon: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: SvgPicture.asset(
-                  "assets/images/img_group_8.svg",
-                  height: 24,
-                  width: 27,
-                ),
-              ),
-              suffixIconConstraints: const BoxConstraints(
-                maxHeight: 48,
-              ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildCreateButton() {
+    return Container(
+      width: double.infinity,
+      height: 48,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF29B6F6),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(22),
+          ),
+        ),
+        onPressed: () {
+          Navigator.pushNamed(context, AppRoutes.connexionScreen);
+        },
+        child: const Text(
+          "Créer",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
       ),
     );
   }
